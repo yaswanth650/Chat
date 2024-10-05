011de7d1f01d0dfa7d9e90ee31ca5bc3cf2a3a7b
yaswanth650 <104341482+yaswanth650@users.noreply.github.com>
2024-10-05 23:43:45 +0530

    Update AbstractChatServlet.java

--- src/main/java/chat/web/servlets/AbstractChatServlet.java
+++ src/main/java/chat/web/servlets/AbstractChatServlet.java
@@ -3,11 +3,11 @@ package chat.web.servlets;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import chat.ChatData;
 import chat.utils.Constants;
 import chat.utils.Security;
