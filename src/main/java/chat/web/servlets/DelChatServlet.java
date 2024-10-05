dd9c423d672e9b253dc2f898976a7a76c841d2aa
yaswanth650 <104341482+yaswanth650@users.noreply.github.com>
2024-10-05 23:48:47 +0530

    Update DelChatServlet.java

--- src/main/java/chat/web/servlets/DelChatServlet.java
+++ src/main/java/chat/web/servlets/DelChatServlet.java
@@ -1,11 +1,11 @@
 package chat.web.servlets;
 
 import java.io.IOException;
-import javax.servlet.Servlet;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import  jakarta.servlet.annotation.WebServlet;
+import  jakarta.servlet.http.HttpServletRequest;
+import  jakarta.servlet.http.HttpServletResponse;
 
 import chat.utils.Constants;
 import chat.web.websockets.ReloadSocket;
